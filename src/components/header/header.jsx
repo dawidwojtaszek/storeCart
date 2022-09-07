8e76676a88ce5674208a1b7b43a7100f09424324
unknown <dawidw22@gmail.com>
2022-09-07 09:16:34 +0200

    feat: create header component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import "./header.scss";
+export const Header = () => (
+  <header>
+    <div className="container">
+      <div className="navigation">
+        <h1>Hello world</h1>
+        <nav>
+          <a>CART</a>
+        </nav>
+      </div>
+    </div>
+  </header>
+);
