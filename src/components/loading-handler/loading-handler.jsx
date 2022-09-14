a4ab76eac291879f9d2b7206d37eff343340c986
unknown <dawidw22@gmail.com>
2022-09-14 10:00:31 +0200

    feat: add loading gif

--- src/components/loading-handler/loading-handler.jsx
+++ src/components/loading-handler/loading-handler.jsx
@@ -1,9 +1,14 @@
 import "./loading-handler.scss";
+import LoadingGif from "../../assets/loading.gif";
+
 export const LoadingHandler = ({ isLoading, isError }) => {
   if (isLoading === true) {
     return (
       <div className="container">
-        <div className="loading-text">Loading...</div>
+        <div className="loading-box">
+          <div className="loading-text">Loading...</div>
+          <img src={LoadingGif} alt="loading" id="loading-gif" />
+        </div>
       </div>
     );
   } else {
